e4c1f2962b28592841fd62de162b276dda63e511
Mustafa Kan <mustafakan242@gmail.com>
2023-12-12 13:57:36 +0300

    States

--- States/src/App.jsx
+++ States/src/App.jsx
@@ -1,11 +1,37 @@
+import React, { useState } from 'react';
+
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("Mustafa")
+  const [friends, setFriends]= useState(["Eda", "Elif"])
+  const [adress, setAdress] = useState({title:"Adana", zip: 10010 })
 
   return (
     <div>
-      
+      <h1>Merhaba {name}</h1> <br />
+      <button onClick={()=> setName("Eda")}>Değiştir</button>
+
+      <hr /> <br />
+
+      <h2>Arkadaşlar</h2>
+      {friends.map((friend, index) => (
+      <div key={index}>{friend}</div>
+      ))} <br />
+
+      <button onClick={()=> setFriends ([...friends, "Hasan"])}>Yeni arkadaş ekle </button>
+
+      <hr /> <br />
+
+      <h2>Adres</h2>
+      <div>
+        {adress.title} {adress.zip}
+      </div><br />
+
+      <button onClick={()=> setAdress ({title:"Mersin", zip: 333333})}>Adres değiştir </button>
+
+
     </div>
   )
 }
