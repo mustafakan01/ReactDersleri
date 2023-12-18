da383163252365ca6cf6ed689bccbed22a2cd4bd
Mustafa Kan <mustafakan242@gmail.com>
2023-12-18 11:10:26 +0300

    Effects

---
+++ ContactApp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Contacs from './components/Contacts/Index'
+
+
+function App() {
+
+  return (
+    <div>
+      <Contacs></Contacs>
+    </div>
+  )
+}
+
+export default App
