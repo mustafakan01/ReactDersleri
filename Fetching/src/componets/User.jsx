f718e9257848f9aa0eeb8190673cc5ea848e0a26
Mustafa Kan <79532179+mustafakan01@users.noreply.github.com>
2023-12-27 14:42:45 +0300

    Fetch

--- Fetching/src/componets/User.jsx
+++ Fetching/src/componets/User.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState , useEffect } from 'react'
+
 
 function User() {
+
+    const [users, setUsers] = useState([])
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((res)=>res.json(res))
+        .then((users)=>setUsers(users))
+    },[]);
+
   return (
-    <div>User</div>
+    <div>
+        <h1>Users</h1>
+        {
+            users.map(user=> <div key={user.id}>{user.name}</div>)
+        }
+    </div>
   )
 }
 
